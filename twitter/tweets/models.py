25dd8560d69eeab1c2b9ce02c142e663f81196d1
sanix sandel <sanicksikani@gmail.com>
2020-06-29 15:55:54 +0300

    like field

--- twitter/tweets/models.py
+++ twitter/tweets/models.py
@@ -4,9 +4,15 @@ import random
 
 User=settings.AUTH_USER_MODEL
 
+class TweetLike(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet=models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp=models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     user=models.ForeignKey(User, related_name='tweets', on_delete=models.CASCADE)
     content=models.TextField(blank=True, null=True)
+    likes=models.ManyToManyField(User, related_name='tweets_liked', blank=True)
     image=models.FileField(upload_to='images/', blank=True, null=True)
     created=models.DateTimeField(auto_now_add=True)
 
@@ -19,3 +25,6 @@ class Tweet(models.Model):
             'content':self.content,
             'likes':random.randint(0, 180)
         }
+
+    def __str__(self):
+        return f'{self.content}'    
