ff22402cf50fe782e8f3a7090d178dfa15e576ae
sanix sandel <sanicksikani@gmail.com>
2020-06-29 10:44:10 +0300

    user

--- twitter/tweets/models.py
+++ twitter/tweets/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.conf import settings
 import random
 
+User=settings.AUTH_USER_MODEL
+
 class Tweet(models.Model):
+    user=models.ForeignKey(User, related_name='tweets', on_delete=models.CASCADE)
     content=models.TextField(blank=True, null=True)
-    image=models.FileField(upload_to='images/', blank=True)
+    image=models.FileField(upload_to='images/', blank=True, null=True)
     created=models.DateTimeField(auto_now_add=True)
 
     class Meta:
