7fce578cc96b65814c34e95cc51e27df6744f739
sanix sandel <sanicksikani@gmail.com>
2020-06-29 08:32:17 +0300

    models serialized

--- twitter/tweets/views.py
+++ twitter/tweets/views.py
@@ -18,7 +18,7 @@ def tweet_create_view(request, *args, **kwargs):
         obj=form.save(commit=False)
         obj.save()
         if request.is_ajax():
-            return JsonResponse({}, status=201) #201==created items
+            return JsonResponse(obj.serialize(), status=201) #201==created items
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
                 
