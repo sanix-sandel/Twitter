580a39af3b5b1bc98844c60ad5cfbabfb9accd37
sanix sandel <sanicksikani@gmail.com>
2020-07-02 10:28:04 +0300

    functionnal compoenents 57

--- twitter/frontend/frontend/src/App.js
+++ twitter/frontend/frontend/src/App.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+function loadTweets(callback){
+  const xhr =new XMLHttpRequest()
+  const method='GET'
+  const url='http://127.0.0.1:8000/tweets/'
+  xhr.responseType="json"
+  xhr.open(method, url)
+  xhr.onload=function(){
+    callback(xhr.response, xhr.status)
+  }
+  xhr.onerror=function(e){
+    console.log(e)
+    callback({"message":"The request was an error"}, 400)
+  }
+  xhr.send()
+}
+
+
+function Tweet(props){
+  const {tweet}=props
+  const className=props.className ? props.className:'col-10 mx-auto col-md-6'
+  return <div>
+    <p>{tweet.id}-{tweet.content}</p>
+  </div>
+}
+
+
+function App (){
+  const [tweets, setTweets]=useState([])
+
+   useEffect(()=>{
+      const myCallback=(response, status)=>{
+       console.log(response, status)
+        if(status===200){
+         setTweets(response)
+        }else{
+         alert("There was an error")
+        }
+      }
+     loadTweets(myCallback)
+   }, [])
+
+  return(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          {tweets.map((item, index)=>{
+            return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`} />
+          })}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +65,10 @@ function App() {
         </a>
       </header>
     </div>
-  );
+
+  ) ;
 }
+
+
 
 export default App;
